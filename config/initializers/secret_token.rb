d3bcd5cd032bd95b0768b099c0ed63ee2b8352ba
vispy homavazir <vispy.homavazir@hotmail.com>
2015-07-15 21:14:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rortraining::Application.config.secret_token = '55724970465b4823f20eb0f509d81db8ae92b928e0b2e64afc688c6df87be8841f92ff5cb1d50d3525697da62d0054ef0a014edae29a24d73fa13f7f9af4e548'
